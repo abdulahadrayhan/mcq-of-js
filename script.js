ffe33019a0d07a7727dfbb20540c90b96e0c9b99
juwel <juwel4495a@gmail.com>
2025-10-06 18:10:48 +0600

    exam: done

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
- let number = [10,21,22,23];
-number.pop()
-console.log(number) 
+/* 21 
+let name = "Rayhan";
+// or 
+let num = 10 ;
+
+
+// 22
+
+
+// 23
+Array - an array can be carry some value at a time .
+
+let numbers =[10,20,30,40 ];
+
+
+// 24
+//function - a function can short our work ,we can use the sm function many time fromm diffrint places . genarally we use function for solve problems.
+
+function vatcalculator (price) {
+  const result = price * 20/100;
+   return result ;
+}
+let bikeprice  = vatcalculator(10000);
+
+console.log(bikeprice); 
+
+// 25 
+for(let i = 0; i <= 20 ; i++) {
+    num += i <br>
+  }
+let num = 20 ;
+console.log(num);
+
+
+
+//26
+let x = 5 ;
+let y = 5 ;
+console.log(x==y, x===y)  
+
+
+// 27 
+
+let arr = [1,2,3];
+arr.pop();
+arr.push(4);
+console.log(arr); 
+
+
+// 28
+let obj = {name:'jhon',age: 25};
+console.log(obj.name); */
